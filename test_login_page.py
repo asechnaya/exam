b06d46726fdc923a483fdff6cc1bbafc1bbb7152
amakarovskaya <anutya@mail.ru>
2022-12-19 21:17:00 +0400

    add allure

--- test_login_page.py
+++ test_login_page.py
@@ -1,27 +1,36 @@
-import time
-
+import allure
 import pytest
 
 from pages.global_variables import FARM, LINK, PASSWORD, USER
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
 
-@pytest.mark.login
+@allure.feature("Login feature")
+@allure.story("user should be able to login to search the cow")
+@allure.title('Negative cases for login page')
+@allure.testcase("https://st.scrdairy.com/", "Разработка автотестов для главной страницы")
+@pytest.mark.xpass
 class TestLoginPage:
     def test_login_with_remember_mark(self, browser):
-        browser.delete_all_cookies()
-        page = LoginPage(browser, LINK)
-        page.open()
-        page.should_be_login_page()
-        page.click_remember_me()
-        page.fill_the_form(USER, PASSWORD, FARM)
-        time.sleep(4)
+        with allure.step("Открываем главную страницу"):
+            browser.delete_all_cookies()
+            page = LoginPage(browser, LINK)
+            page.open()
+            page.should_be_login_page()
+            page.click_remember_me()
+            page.fill_the_form(USER, PASSWORD, FARM)
+            main_page = MainPage(browser, browser.current_url)
+            main_page.should_be_main_page()
 
     def test_login_without_remember_mark(self, browser):
         browser.delete_all_cookies()
         page = LoginPage(browser, LINK)
         page.open()
         page.should_be_login_page()
+        page.fill_the_form(USER, PASSWORD, FARM)
+        main_page = MainPage(browser, browser.current_url)
+        main_page.should_be_main_page()
 
     def test_password_should_be_not_visible(self, browser):
         browser.delete_all_cookies()
@@ -29,3 +38,6 @@ class TestLoginPage:
         page.open()
         page.should_be_login_page()
         page.password_is_invisible()
+        page.fill_the_form(USER, PASSWORD, FARM)
+        main_page = MainPage(browser, browser.current_url)
+        main_page.should_be_main_page()
